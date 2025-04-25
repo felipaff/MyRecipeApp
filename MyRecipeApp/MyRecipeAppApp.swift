2c8ba4c15d9cc81a6e59e0e4a77b0b86b827cef2
Felipe Peña <felipaff@gmail.com>
2025-04-24 22:55:06 -0400

    Initial Commit

---
+++ MyRecipeApp/MyRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipeAppApp.swift
+//  MyRecipeApp
+//
+//  Created by Felipe Peña on 24-04-25.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyRecipeAppTests/MyRecipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipeAppTests.swift
+//  MyRecipeAppTests
+//
+//  Created by Felipe Peña on 24-04-25.
+//
+
+import Testing
+@testable import MyRecipeApp
+
+struct MyRecipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
